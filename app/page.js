ca03e5f2c49212a7cfb75c66165b2e5d27dfca14
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2024-03-11 12:40:50 +0500

    todo app completed

--- app/page.js
+++ app/page.js
@@ -4,11 +4,38 @@ import React, { useState } from "react";
 const page = () => {
   const [title, settitle] = useState("");
   const [desc, setdesc] = useState("");
+  const [mainTask, setmainTask] = useState([]);
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log(title);
-    console.log(desc);
+    setmainTask([...mainTask, { title, desc }]);
+    settitle("");
+    setdesc("");
+    console.log(mainTask);
   };
+  const deleteHandler = (i) => {
+    let copytask = [...mainTask];
+    copytask.splice(i, 1);
+    setmainTask(copytask);
+  };
+  let renderTask = <h2>No Task Available</h2>;
+  if (mainTask.length > 0) {
+    renderTask = mainTask.map((t, i) => {
+      return (
+        <li key={i} className="flex justify-between  items-center">
+          <div className="flex justify-between w-1/3 m-5">
+            <h5 className="text-2xl font-semibold">{t.title}</h5>
+            <h6 className="text-xl font-semibold">{t.desc}</h6>
+          </div>
+          <button
+            onClick={deleteHandler}
+            className="bg-red-400 text-white px-4 py-2 rounded font-bold hover:bg-red-600"
+          >
+            Delete
+          </button>
+        </li>
+      );
+    });
+  }
   return (
     <>
       <h1 className="bg-black p-5 text-white text-5xl font-bold text-center">
@@ -44,6 +71,10 @@ const page = () => {
           Add Task
         </button>
       </form>
+      <hr />
+      <div className="p-8 bg-slate-200">
+        <ul>{renderTask}</ul>
+      </div>
     </>
   );
 };
